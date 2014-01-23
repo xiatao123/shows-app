cbe1e4caef4081089cecb9a31f59b6f9b1bbbfa4
sudocode <dspark@yahoo-inc.com>
2014-01-22 21:53:24 -0800

    added yahoo authentication and wireframes for views

---
+++ YahooClient.m
@@ -0,0 +1,13 @@
+//
+//  YahooClient.m
+//  showsApp
+//
+//  Created by Daniel Park on 1/22/14.
+//  Copyright (c) 2014 Shows. All rights reserved.
+//
+
+#import "YahooClient.h"
+
+@implementation YahooClient
+
+@end
--- showsApp/ViewController.m
+++ showsApp/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "AFOAuth1Client.h"
 
 @interface ViewController ()
+- (IBAction)onLoginTapped:(UIButton *)sender;
+@property (nonatomic, strong) AFOAuth1Client *yahoo;
 
 @end
 
@@ -16,8 +19,15 @@
 
 - (void)viewDidLoad
 {
+    
+    NSString *key = @"";
+    NSString *secret = @"";
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"view did load");
+    self.yahoo = [[AFOAuth1Client alloc] initWithBaseURL:[NSURL URLWithString:@"https://api.login.yahoo.com/oauth/v2/"] key:key secret:secret];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +36,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onLoginTapped:(UIButton *)sender {
+    NSLog(@"login button tapped");
+    [self.yahoo authorizeUsingOAuthWithRequestTokenPath:@"get_request_token" userAuthorizationPath:@"request_auth" callbackURL:[NSURL URLWithString:@"yahoo://success"] accessTokenPath:@"get_token" accessMethod:@"POST" scope:nil success:^(AFOAuth1Token *access_token, id request){
+        NSLog(@"success");
+    } failure:^(NSError *error) {
+        NSLog(@"failure");
+        NSLog(@"%@", error);
+    }];
+}
 @end
