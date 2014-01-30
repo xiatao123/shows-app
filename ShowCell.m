8ad514af7524e2ace931a281ad1b01b369bc1098
MasonPD <xilinkai@gmail.com>
2014-01-29 19:18:07 -0800

    add collection view controller and cutom cell

---
+++ ShowCell.m
@@ -0,0 +1,31 @@
+//
+//  ShowCell.m
+//  showsApp
+//
+//  Created by Linkai Xi on 1/29/14.
+//  Copyright (c) 2014 Shows. All rights reserved.
+//
+
+#import "ShowCell.h"
+
+@implementation ShowCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
