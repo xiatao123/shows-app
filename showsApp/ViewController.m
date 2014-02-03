38128a1c7774065bc807d799cfa5623373d09822
sudocode <dspark@yahoo-inc.com>
2014-02-03 12:09:39 -0800

    rearranging stuff

--- showsApp/ViewController.m
+++ showsApp/ViewController.m
@@ -15,7 +15,7 @@
 @interface ViewController ()
 - (IBAction)onLoginTapped:(UIButton *)sender;
 @property (nonatomic, strong) AFOAuth1Client *yahoo;
-
+@property (nonatomic, strong) NSUserDefaults * defaults;
 @end
 
 @implementation ViewController
@@ -23,10 +23,11 @@
 - (void)viewDidLoad
 {
     
-    NSString *key = @"";
-    NSString *secret = @"";
+    NSString *key = @"dj0yJmk9aks5ZHdkNzcxZ2dFJmQ9WVdrOVYwUnNPR0pRTkdVbWNHbzlPRFEzTWpVNE5EWXkmcz1jb25zdW1lcnNlY3JldCZ4PWVl";
+    NSString *secret = @"1867229fe956948e06b3d2958a6fe973ab052c4e";
     
     [super viewDidLoad];
+    self.defaults = [NSUserDefaults standardUserDefaults];
 	// Do any additional setup after loading the view, typically from a nib.
     NSLog(@"view did load");
     self.yahoo = [[AFOAuth1Client alloc] initWithBaseURL:[NSURL URLWithString:@"https://api.login.yahoo.com/oauth/v2/"] key:key secret:secret];
@@ -37,7 +38,10 @@
      */
     
     [[YQL use:@{@"store://lsri0aFyNSXQsSFK0jYL9F": @"tvdb" }]
-     select:@"*" from:@"tvdb" where:@{ @"date" : @"20140114" } callback:^(NSError *error, id response) {
+     select:@"*"
+     from:@"tvdb"
+     where:@{ @"date" : @"20140114" }
+     callback:^(NSError *error, id response) {
          
          //NSLog(@"got resposne %@", response);
          NSLog(@"get response.result %@", [response valueForKeyPath:@"query.results.results"] );
@@ -49,8 +53,13 @@
          Show *show = [showResult.shows objectAtIndex:0];
          NSLog(@"0 show name %@", show.title);
      }
-     ];
+    ];
     
+    NSData * data = [[self defaults] objectForKey:@"accessToken"];
+    if (data) {
+        AFOAuth1Token * accessToken = [NSKeyedUnarchiver unarchiveObjectWithData:data];;
+        [self performSegueWithIdentifier:@"goToHome" sender:self];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -60,12 +69,30 @@
 }
 
 - (IBAction)onLoginTapped:(UIButton *)sender {
-    NSLog(@"login button tapped");
-    [self.yahoo authorizeUsingOAuthWithRequestTokenPath:@"get_request_token" userAuthorizationPath:@"request_auth" callbackURL:[NSURL URLWithString:@"yahoo://success"] accessTokenPath:@"get_token" accessMethod:@"POST" scope:nil success:^(AFOAuth1Token *access_token, id request){
-        NSLog(@"success");
-    } failure:^(NSError *error) {
-        NSLog(@"failure");
-        NSLog(@"%@", error);
-    }];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    [self.yahoo
+     authorizeUsingOAuthWithRequestTokenPath:@"get_request_token"
+     userAuthorizationPath:@"request_auth"
+     callbackURL:[NSURL URLWithString:@"yahoo://success"]
+     accessTokenPath:@"get_token"
+     accessMethod:@"POST"
+     scope:nil success:^(AFOAuth1Token *accessToken, id request){
+         [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+         
+         // encode access token
+         NSData *data = [NSKeyedArchiver archivedDataWithRootObject:accessToken];
+         
+         // save to NSUserDefaults
+         [[self defaults] setObject:data forKey:@"accessToken"];
+         [[self defaults] synchronize];
+         
+         [self performSegueWithIdentifier:@"goToHome" sender:self];
+     }
+     failure:^(NSError *error) {
+         [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+         // popup for error
+         [[[UIAlertView alloc] initWithTitle:@"Error" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil] show];
+     }
+    ];
 }
 @end
